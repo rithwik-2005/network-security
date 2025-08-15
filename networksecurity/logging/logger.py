ee1c2c3e9e61aa3b4f2238f4907e637556686d5a
rithwik <rithwikvasa@gmail.com>
2025-08-15 15:18:55 +0530

    logging and exception.py file are ready

---
+++ networksecurity/logging/logger.py
@@ -0,0 +1,14 @@
+import logging
+import os
+from datetime import datetime
+
+Log_file=f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log" #log file name or address
+logs_path=os.path.join(os.getcwd(),"logs")
+os.makedirs(logs_path,exist_ok=True)
+LOG_FILE_PATH=os.path.join(logs_path,Log_file)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
+)
